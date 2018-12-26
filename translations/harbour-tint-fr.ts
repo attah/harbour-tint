88b0bf49b736222095c387fac112579ffc5eb42b
Quentí <33203663+Quenty31@users.noreply.github.com>
2018-12-26 17:52:30 +0100

    French translation

---
+++ translations/harbour-tint-fr.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>BridgePage</name>
+    <message>
+        <source>Rooms</source>
+        <translation>Salles</translation>
+    </message>
+    <message>
+        <source>Lights</source>
+        <translation>Lumières</translation>
+    </message>
+    <message>
+        <source>New group</source>
+        <translation>Nouveau groupe</translation>
+    </message>
+    <message>
+        <source>Bridge properties</source>
+        <translation>Propriétés du pont</translation>
+    </message>
+    <message>
+        <source>Global</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <source>Edit group</source>
+        <translation>Modifier le groupe</translation>
+    </message>
+    <message>
+        <source>Rename group</source>
+        <translation>Renommer le groupe</translation>
+    </message>
+    <message>
+        <source>Delete group</source>
+        <translation>Supprimer le groupe</translation>
+    </message>
+    <message>
+        <source>Deleting group</source>
+        <translation>Suppression du groupe</translation>
+    </message>
+    <message>
+        <source>Ct</source>
+        <translation>Ct</translation>
+    </message>
+    <message>
+        <source>Hue</source>
+        <translation>Teinte</translation>
+    </message>
+    <message>
+        <source>Sat</source>
+        <translation>Sat</translation>
+    </message>
+</context>
+<context>
+    <name>BridgePropertiesPage</name>
+    <message>
+        <source>Install updates</source>
+        <translation>Installer les mises à jour</translation>
+    </message>
+    <message>
+        <source>Check for updates</source>
+        <translation>Vérifier les mises à jour</translation>
+    </message>
+    <message>
+        <source>Installing updates</source>
+        <translation>Installation des mises à jour</translation>
+    </message>
+    <message>
+        <source>No updates available</source>
+        <translation>Aucune mise à jour</translation>
+    </message>
+    <message>
+        <source>Transferring</source>
+        <translation>Transfert</translation>
+    </message>
+    <message>
+        <source>All ready to install</source>
+        <translation>Toutes prêtes à l’installation</translation>
+    </message>
+    <message>
+        <source>Some ready to install</source>
+        <translation>Certaines prêtes à l’installation</translation>
+    </message>
+    <message>
+        <source>Installing</source>
+        <translation>Installation</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <source>Bridge properties</source>
+        <translation>Propriétés du pont</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Model</source>
+        <translation>Modèle</translation>
+    </message>
+    <message>
+        <source>Updates</source>
+        <translation>Mises à jour</translation>
+    </message>
+    <message>
+        <source>Software version</source>
+        <translation>Version logicielle</translation>
+    </message>
+    <message>
+        <source>IP address</source>
+        <translation>Adresse IP</translation>
+    </message>
+    <message>
+        <source>MAC address</source>
+        <translation>Adresse MAC</translation>
+    </message>
+    <message>
+        <source>Bridge id</source>
+        <translation>ID du pont</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Tint</source>
+        <translation>Tint</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Available bridges</source>
+        <translation>Ponts disponibles</translation>
+    </message>
+    <message>
+        <source>No bridges found</source>
+        <translation>Aucun pont trouvé</translation>
+    </message>
+    <message>
+        <source>Unpair</source>
+        <translation>Dépareiller</translation>
+    </message>
+    <message>
+        <source>Bridge</source>
+        <translation>Pont</translation>
+    </message>
+</context>
+<context>
+    <name>LightsPage</name>
+    <message>
+        <source>Lights</source>
+        <translation>Lumières</translation>
+    </message>
+    <message>
+        <source>New Lights</source>
+        <translation>Nouvelles lumières</translation>
+    </message>
+    <message>
+        <source>Rename light</source>
+        <translation>Renommer la lumière</translation>
+    </message>
+    <message>
+        <source>Delete light</source>
+        <translation>Supprimer la lumière</translation>
+    </message>
+    <message>
+        <source>Deleting light</source>
+        <translation>Suppression de la lumière</translation>
+    </message>
+</context>
+<context>
+    <name>NewLightsPage</name>
+    <message>
+        <source>New Lights</source>
+        <translation>Nouvelles lumières</translation>
+    </message>
+    <message>
+        <source>Search again</source>
+        <translation>Chercher à nouveau</translation>
+    </message>
+</context>
+<context>
+    <name>PairDialog</name>
+    <message>
+        <source>Pairing</source>
+        <translation>Association en cours</translation>
+    </message>
+    <message>
+        <source>Press the pairing button</source>
+        <translation>Appuyez sur le nouveau d’association</translation>
+    </message>
+    <message>
+        <source>Success!</source>
+        <translation>Réussi !</translation>
+    </message>
+</context>
+<context>
+    <name>RenameGroupDialog</name>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+</context>
+</TS>
