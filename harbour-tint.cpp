0958a98dd32891a482bb2af56f6f287d456e6eeb
Anton Thomasson <antonthomasson@gmail.com>
2021-12-01 20:07:08 +0100

    Enable SailJail and add settings migration

--- harbour-tint.cpp
+++ harbour-tint.cpp
@@ -1,19 +1,53 @@
 #include <QtQuick>
+#include <QCryptographicHash>
 #include <sailfishapp.h>
 #include <huediscovery.h>
 
+void migrateLocalStorage()
+{
+    // The new location of the LocalStorage database
+    QDir newDbDir(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/net.attah/tint/QML/OfflineStorage/Databases/");
+
+    if(newDbDir.exists())
+        return;
+
+    newDbDir.mkpath(newDbDir.path());
+
+    QString dbname = QString(QCryptographicHash::hash(("TintDB"), QCryptographicHash::Md5).toHex());
+    // The new LocalStorage database
+    QFile newDb(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/net.attah/tint/QML/OfflineStorage/Databases/" + dbname + ".sqlite");
+    QFile newIni(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/net.attah/tint/QML/OfflineStorage/Databases/" + dbname + ".ini");
+
+    // The old LocalStorage database
+    QFile oldDb(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/harbour-tint/harbour-tint/QML/OfflineStorage/Databases/" + dbname + ".sqlite");
+    QFile oldIni(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/harbour-tint/harbour-tint/QML/OfflineStorage/Databases/" + dbname + ".ini");
+
+    oldDb.open(QFile::ReadOnly);
+    oldIni.open(QFile::ReadOnly);
+    newDb.open(QFile::WriteOnly);
+    newIni.open(QFile::WriteOnly);
+
+    newDb.write(oldDb.readAll());
+    newIni.write(oldIni.readAll());
+}
+
+
 int main(int argc, char *argv[])
 {
+    migrateLocalStorage();
+
+    QGuiApplication* app = SailfishApp::application(argc, argv);
+
+    app->setOrganizationName(QStringLiteral("net.attah"));
+    app->setApplicationName(QStringLiteral("tint"));
+
     qmlRegisterType<HueDiscovery>("tint.huediscovery", 1, 0, "HueDiscoveryModel");
-    // SailfishApp::main() will display "qml/cpp-test.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
+
+    QQuickView* view = SailfishApp::createView();
+
+    view->setSource(SailfishApp::pathToMainQml());
+    view->show();
+    return app->exec();
 
     return SailfishApp::main(argc, argv);
 }
